132c00e65c7536e6027f85070a809a0a2acd95da
AdarshMishra27 <60168931+AdarshMishra27@users.noreply.github.com>
2021-10-11 22:07:03 +0530

    Stud Info System

---
+++ studentInformationSystem/StudentInfo.java
@@ -0,0 +1,21 @@
+package studentInformationSystem;
+
+public class StudentInfo {
+	
+	int roll;
+	String name;
+	String gender;
+	long phone;
+
+	public StudentInfo(int roll, String name, String gender, Long phone) {
+		this.roll=roll;
+		this.name=name;
+		this.gender=gender;
+		this.phone=phone;
+	}
+	
+	public StudentInfo() {
+		
+	}
+	
+}
